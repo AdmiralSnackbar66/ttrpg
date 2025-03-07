d71034dbc96335a2c7757b29f4268ae9fc8efe48
AdmiralSnackbar66 <seanmcdine@gmail.com>
2025-03-07 19:56:33 +0000

    Last Sync: 2025-03-07 19:56:33 (Mobile)

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,6 +1,9 @@
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
 
+
+Component.RecentNotes({ title: "Recent blog posts" })
+
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
